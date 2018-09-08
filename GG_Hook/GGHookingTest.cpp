e968779955b10629fef1d5024f6f3353bf7f4998
shekohex <shekohex@gmail.com>
2018-09-08 03:15:34 +0200

    adding new class test

---
+++ GG_Hook/GGHookingTest.cpp
@@ -0,0 +1,31 @@
+#include "GGHookingTest.h"
+
+namespace COGG {
+	GGHookingTest::GGHookingTest()
+	{
+	}
+
+
+	GGHookingTest::~GGHookingTest()
+	{
+	}
+
+	void COGG::GGHookingTest::SetupHook()
+	{
+	}
+
+	void COGG::GGHookingTest::OnHookInit()
+	{
+	}
+
+	void COGG::GGHookingTest::OnHookDestroy()
+	{
+	}
+
+	::std::string COGG::GGHookingTest::GetHookName()
+	{
+		return ::std::string();
+	}
+}
+
+
